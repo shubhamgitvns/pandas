688af62f884acbfac17eb15885759f0d23a30f57
shubhamgitvns <shubhammaurya10092002@gmail.com>
2026-06-13 14:44:13 +0530

    Create the messy dataframe

---
+++ basic_txt_cleaning/create_dataframe.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+
+data = {
+    "Student": ["a" , "b", "c", "d", "e", "f", "g"],
+    "Math": [70, 75, None, 38, 78, 110, 60],
+    "Chem": [75, 28, 85, None, 78, 65, 40],
+    "Phy": [None, None, 88, 158, 77, 68, 75]
+
+}
+
+df = pd.DataFrame(data)
+print(df)
